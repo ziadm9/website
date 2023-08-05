dac542faa6e3f36e35f44e12298eb00c7236f992
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-08-05 16:21:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function myFunction(x) {
     if (x.matches) {
       document.querySelectorAll("#moreAbout")[0].style.display = "inline"
       document.querySelectorAll("#moreAbout")[1].style.display = "none"
-      console.log("kys")
     } else {
         document.querySelectorAll("#moreAbout")[1].style.display = "none"
         document.querySelectorAll("#moreAbout")[0].style.display = "flex"
-        console.log("kys23")
     }
   }
   
-  x = window.matchMedia("(max-width: 600px)")
+  x = window.matchMedia("(max-width: 768px)")
   myFunction(x) 
   x.addListener(myFunction)
   
